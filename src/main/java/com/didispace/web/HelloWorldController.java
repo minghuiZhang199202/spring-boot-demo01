311772a834e089b1f0e964e7c2794796ec94e7ab
zhang hui <yuchen199202@163.com>
2017-12-07 22:49:12 +0800

    test

--- src/main/java/com/didispace/web/HelloWorldController.java
+++ src/main/java/com/didispace/web/HelloWorldController.java
@@ -1,8 +1,16 @@
 package com.didispace.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author minghui_zhang
  */
+@RestController
 public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello,World!";
+    }
 
 }
